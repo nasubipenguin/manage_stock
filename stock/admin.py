b0562883c8f7587f6cfcc9a634b5989741c583e5
Satoko Nasu <harayan01@gmail.com>
2019-11-04 22:21:21 +0900

    Setting admin.

--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Stock
+from .models import Performance
+from .models import Shikiho
+
+admin.site.register(Stock)
+admin.site.register(Performance)
+admin.site.register(Shikiho)
