ca982695b8eb7cdbf5132ca72e568f99dfaa4688
Satoko Nasu <harayan01@gmail.com>
2019-11-04 22:45:15 +0900

    Set views.

--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+def stock_list(request):
+    return render(request, 'blog/stock_list.html', {})
+
+def stock_new(request):
+    return render(request, 'blog/stock_new.html', {})
+
+def stock_info(request):
+    return render(request, 'blog/stock_info.html', {})
+
+def stock_edit(request):
+    return render(request, 'blog/stock_edit.html', {})
+
+def stock_detail(request):
+    return render(request, 'blog/stock_detail.html', {})
+
+def performance_new(request):
+    return render(request, 'blog/performance_new.html', {})
+
+def performance_info(request):
+    return render(request, 'blog/performance_info.html', {})
+
+def performance_edit(request):
+    return render(request, 'blog/performance_edit.html', {})
+
+def shikiho_new(request):
+    return render(request, 'blog/shikiho_new.html', {})
+
+def shikiho_info(request):
+    return render(request, 'blog/shikiho_info.html', {})
+
+def shikiho_edit(request):
+    return render(request, 'blog/shikiho_edit.html', {})
